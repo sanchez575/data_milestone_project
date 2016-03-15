9094c604b46a07ab85a0971dac52f1581fbe7def
Juan Manuel Sanchez <jm.sanchez@berkeley.edu>
2016-03-14 20:03:34 -0700

    add exception handling

--- app.py
+++ app.py
@@ -30,36 +30,40 @@ def graph():
 	from bokeh.embed import components
 
 	q_key = 'GzvNgrVEuh3HdRwksr5e'
-	stock = app.vars['stock']
+	stock = app.vars['stock'].upper()
 	num_points = 300
 	show_this = app.vars['options']
 	#show_this = ['Open','Close']
 	colors = ['#C4CDE9','#FFFEC9','#FBD4DA','#EED2F0']
 
-	r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/%s.csv?api_key=%s'%(stock,q_key))
-	df = pd.read_csv(StringIO(r.text),header=0,parse_dates=['Date'])
+	try:
 
-	path = '/Users/juanmanuelsanchez/Documents/data_science_incubator/heroku-stuff/'
-	dates = df.Date[:num_points]
-	TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+		r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/%s.csv?api_key=%s'%(stock,q_key))
+		df = pd.read_csv(StringIO(r.text),header=0,parse_dates=['Date'])
 
-	r = figure(x_axis_type = "datetime",tools=TOOLS)
+		path = '/Users/juanmanuelsanchez/Documents/data_science_incubator/heroku-stuff/'
+		dates = df.Date[:num_points]
+		TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 
-	for ticker, color in zip(show_this,colors[:len(show_this)]):
-	    y = df[ticker][:num_points]
-	    r.line(dates, y, color=color, legend = stock + ': ' + ticker)
+		r = figure(x_axis_type = "datetime",tools=TOOLS)
 
-	output_file(path + "correlation.html", title="correlation.py example")
+		for ticker, color in zip(show_this,colors[:len(show_this)]):
+		    y = df[ticker][:num_points]
+		    r.line(dates, y, color=color, legend = stock + ': ' + ticker)
 
-	r.title = "Stock Returns. Data from Quandl"
-	r.grid.grid_line_alpha=0.3
+		output_file(path + "correlation.html", title="correlation.py example")
 
-	from bokeh.resources import CDN
-	from bokeh.embed import file_html
+		r.title = "Stock Returns. Data from Quandl"
+		r.grid.grid_line_alpha=0.3
 
-	script, div = components(r)
+		from bokeh.resources import CDN
+		from bokeh.embed import file_html
 
-	return render_template('graph.html',div=div,script=script)
+		script, div = components(r)
+
+		return render_template('graph.html',div=div,script=script)
+	except:
+		return render_template('graph.html',div="<div><h1>Something went wrong! (e.g. no ticker with that name)</h1></div>",script='')
 	#app.vars['html'] = html
 
 if __name__ == '__main__':
